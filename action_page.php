7e9cd5852d1f2b0884148d5986a665f9cafa825a
angeltee123 <45322441+angeltee123@users.noreply.github.com>
2023-08-03 10:01:05 -0500

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE FOLLOWING TWO LINES:
-    $email_to = "you@yourdomain.com";
+    $email_to = "angelteee1498@gmail.com";
     $email_subject = "New form submissions";
 
     function problem($error)
